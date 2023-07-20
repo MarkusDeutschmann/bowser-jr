ccca6e9ed61a6b692fe62b0c9f1be8b06d7a643f
Markus Deutschmann <Markus.Deutschmann@gfk.com>
2023-07-20 17:27:57 +0200

    just holding the dist part now

--- packages/core/src/engine/engineMap.ts
+++
@@ -1,8 +0,0 @@
-export const ENGINE_MAP = {
-  EdgeHTML: "EdgeHTML",
-  Blink: "Blink",
-  Trident: "Trident",
-  Presto: "Presto",
-  Gecko: "Gecko",
-  WebKit: "WebKit",
-};
--- packages/core/src/platform/platformMap.ts
+++
@@ -1,6 +0,0 @@
-export const PLATFORMS_MAP = {
-  tablet: "tablet",
-  mobile: "mobile",
-  desktop: "desktop",
-  tv: "tv",
-};
